a98825f18cc0e21500ef2cd733cfe75f09ff9093
SnotlingLiu <snotlingliu@gmail.com>
2023-06-26 11:52:08 +0800

    Chore: 删除冗余数据

--- Source/Shaders/Builtin/Structs/modelVertexOutput.js
+++
@@ -1,6 +0,0 @@
-//This file is automatically rebuilt by the Cesium build process.
-export default "struct czm_modelVertexOutput {\n\
-vec3 positionMC;\n\
-float pointSize;\n\
-};\n\
-";
--- Source/Shaders/Builtin/Structs/pbrParameters.js
+++
@@ -1,8 +0,0 @@
-//This file is automatically rebuilt by the Cesium build process.
-export default "struct czm_pbrParameters\n\
-{\n\
-vec3 diffuseColor;\n\
-float roughness;\n\
-vec3 f0;\n\
-};\n\
-";
